368565508bf5d6387eadf8836cf0b65fa58ec47b
AYUSH MAHADEO WASE <98109454+AyushAI@users.noreply.github.com>
2025-10-05 12:15:46 +0530

    Chatmodel Openai

---
+++ LANGCHAINS MODELS/CHAT_MODELS/ChatModels_using_openai.py
@@ -0,0 +1,11 @@
+from langchain_openai import chat_models
+from dotenv import load_dotenv
+
+load_dotenv()
+
+model = ChatOpenAI(model='gpt-4')
+
+result = model.invoke("Explain ML to a child in easy and fun way")
+
+print(result.content)
+
